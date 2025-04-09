1415a63a51e48df3fc193393bca84c1a87185bb3
samypt <vitaliiptichkin@gmail.com>
2025-04-09 15:13:32 +0200

    Add update project route

--- app/routers/projects.py
+++ app/routers/projects.py
@@ -6,10 +6,11 @@ from app.database import get_session
 
 
 router = APIRouter(prefix="/projects", tags=["Projects"])
+db_session: Session = Depends(get_session)
 
 
 @router.post("/", response_model=ProjectRead)
-async def create_project(project: ProjectCreate, session: Session = Depends(get_session)):
+async def create_project(project: ProjectCreate, session: db_session):
     statement = select(Project).where(
         or_(
             Project.name == project.name,
@@ -28,7 +29,7 @@ async def create_project(project: ProjectCreate, session: Session = Depends(get_
 
 
 @router.get("/{project_name}", response_model=ProjectRead)
-async def get_project(project_name: str, session: Session = Depends(get_session)):
+async def get_project(project_name: str, session: db_session):
     statement = select(Project).where(Project.name == project_name)
     project_to_get = session.exec(statement).first()
     if not project_to_get:
@@ -37,10 +38,25 @@ async def get_project(project_name: str, session: Session = Depends(get_session)
 
 
 @router.delete("/delete/{project_id}", status_code=204)
-async def delete_project(project_id, session: Session = Depends(get_session))
+async def delete_project(project_id, session: db_session):
     project_to_delete = session.get(Project, project_id)
     if not project_to_delete:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="Project not found")
     session.delete(project_to_delete)
     session.commit()
     return
+
+
+@router.put("/project/{project_name}", response_model=ProjectRead)
+async def update_project(project_name: str, project: ProjectUpdate, session: db_session):
+    statement = select(Project).where(Project.name == project_name)
+    project_to_update = session.exec(statement).first()
+    if not project_to_update:
+        raise HTTPException(status_code=404, detail="Project not found")
+    data_to_update = project.model_dump()
+    for key, value in data_to_update:
+        setattr(project_to_update, key, value)
+    session.add(project_to_update)
+    session.commit()
+    session.refresh(project_to_update)
+    return project_to_update
