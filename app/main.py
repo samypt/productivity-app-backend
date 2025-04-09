7a4c62c9d43ccd042935597a6aa7f26087097588
samypt <vitaliiptichkin@gmail.com>
2025-04-09 15:41:43 +0200

    Add project routes

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from app.routers.users import router as user_router
 from app.routers.teams import router as team_router
+from app.routers.projects import router as project_router
 
 app = FastAPI()
 
 app.include_router(user_router, prefix="/api")
 app.include_router(team_router, prefix="/api")
+app.include_router(project_router, prefix="/api")
 
