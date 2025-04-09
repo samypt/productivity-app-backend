bfd3b828ba73994e8a9e0401813f50188650c131
samypt <vitaliiptichkin@gmail.com>
2025-04-09 15:42:33 +0200

    Improve routes

--- app/routers/projects.py
+++ app/routers/projects.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
-from sqlmodel import Session, select, or_
-from app.models import Project
+from sqlmodel import Session, select, and_
+from app.models import Project, Team
 from app.schemas.project import ProjectRead, ProjectUpdate, ProjectCreate
 from app.database import get_session
 
@@ -10,9 +10,9 @@ db_session: Session = Depends(get_session)
 
 
 @router.post("/", response_model=ProjectRead)
-async def create_project(project: ProjectCreate, session: db_session):
+async def create_project(project: ProjectCreate, session = db_session):
     statement = select(Project).where(
-        or_(
+        and_(
             Project.name == project.name,
             Project.team_id == project.team_id
         )
@@ -20,7 +20,11 @@ async def create_project(project: ProjectCreate, session: db_session):
     existing_project = session.exec(statement).first()
     if existing_project:
         raise HTTPException(status_code=409,
-                            detail="Project with this name already exists")
+                            detail="Project with this name already exists"
+                                   " in this team")
+    existing_team = session.get(Team, project.team_id)
+    if not existing_team:
+        raise HTTPException(status_code=404, detail="Team not found")
     new_project = Project(**project.model_dump())
     session.add(new_project)
     session.commit()
@@ -28,17 +32,16 @@ async def create_project(project: ProjectCreate, session: db_session):
     return new_project
 
 
-@router.get("/{project_name}", response_model=ProjectRead)
-async def get_project(project_name: str, session: db_session):
-    statement = select(Project).where(Project.name == project_name)
-    project_to_get = session.exec(statement).first()
+@router.get("/{project_id}", response_model=ProjectRead)
+async def get_project(project_id: str, session = db_session):
+    project_to_get = session.get(Project, project_id)
     if not project_to_get:
         raise HTTPException(status_code=404, detail="Project not found")
     return project_to_get
 
 
 @router.delete("/delete/{project_id}", status_code=204)
-async def delete_project(project_id, session: db_session):
+async def delete_project(project_id, session = db_session):
     project_to_delete = session.get(Project, project_id)
     if not project_to_delete:
         raise HTTPException(status_code=404, detail="Project not found")
@@ -47,14 +50,13 @@ async def delete_project(project_id, session: db_session):
     return
 
 
-@router.put("/project/{project_name}", response_model=ProjectRead)
-async def update_project(project_name: str, project: ProjectUpdate, session: db_session):
-    statement = select(Project).where(Project.name == project_name)
-    project_to_update = session.exec(statement).first()
+@router.put("/project/{project_id}", response_model=ProjectRead)
+async def update_project(project_id: str, project: ProjectUpdate, session = db_session):
+    project_to_update = session.get(Project, project_id)
     if not project_to_update:
         raise HTTPException(status_code=404, detail="Project not found")
     data_to_update = project.model_dump()
-    for key, value in data_to_update:
+    for key, value in data_to_update.items():
         setattr(project_to_update, key, value)
     session.add(project_to_update)
     session.commit()
