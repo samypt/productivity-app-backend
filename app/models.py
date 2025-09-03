b2b984095d1813ca96b8896d13e59d84b7be7779
samypt <vitaliiptichkin@gmail.com>
2025-09-03 14:17:28 +0200

    Add Invite, Notification, GoogleIvent and Links Event<->User<->Task models

--- app/models.py
+++ app/models.py
@@ -1,13 +1,10 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 from sqlalchemy import (UniqueConstraint, CheckConstraint,
-                        Column, String, Integer, ForeignKey)
+                        Column, String, Integer, ForeignKey, event)
 from sqlmodel import SQLModel, Field, Relationship
 from typing import Optional, List
 from uuid import uuid4
-
-
-def get_time_stamp():
-    return datetime.now(timezone.utc)
+from .utils.time import get_time_stamp
 
 
 class User(SQLModel, table=True):
@@ -19,15 +16,27 @@ class User(SQLModel, table=True):
     last_name: str = Field(index=True, max_length=255)
     hashed_password: str
     created_at: datetime = Field(default_factory=get_time_stamp)
+    updated_at: datetime = Field(default_factory=get_time_stamp)
     role: str = Field(
         default="member",
         max_length=50,
         sa_column=Column(String(50), CheckConstraint("role IN ('admin', 'member')"))
     )
+    avatar_url: Optional[str] = Field(default=None, index=True, unique=True)
+
+    # New fields for Google Calendar integration
+
+    google_access_token: Optional[str] = None
+    google_refresh_token: Optional[str] = None
+    google_token_expiry: Optional[str] = None  # ISO timestamp or datetime
+    google_calendar_id: Optional[str] = Field(index=True, unique=True)
+    google_calendar_html_link: Optional[str] = None
 
     # Relationships
     teams: List["Member"] = Relationship(back_populates='user', passive_deletes=True)
     events: List["Event"] = Relationship(back_populates='user', passive_deletes=True)
+    invites_sent: List["Invite"] = Relationship(back_populates="inviter", passive_deletes=True)
+    google_events: List["GoogleSyncedEvent"] = Relationship(back_populates="user", passive_deletes=True)
 
 
 
@@ -41,10 +50,12 @@ class Team(SQLModel, table=True):
                       sa_column_kwargs={"nullable": False})
     description: Optional[str] = Field(default=None)
     created_at: datetime = Field(default_factory=get_time_stamp)
+    updated_at: datetime = Field(default_factory=get_time_stamp)
 
     # Relationships
     members: List["Member"] = Relationship(back_populates='team', passive_deletes=True)
     projects: List["Project"] = Relationship(back_populates='team', passive_deletes=True)
+    invites_sent_by_members: List["Invite"] = Relationship(back_populates="team", passive_deletes=True)
 
 
 
@@ -66,7 +77,7 @@ class TaskMemberLink(SQLModel, table=True):
 
 
 
-class EvenMemberLink(SQLModel, table=True):
+class EventMemberLink(SQLModel, table=True):
     __tablename__ = 'event_member_link'
     id: str = Field(default_factory=lambda: str(uuid4()), primary_key=True)
     event_id: str = Field(
@@ -98,6 +109,8 @@ class Member(SQLModel, table=True):
         max_length=50,
         sa_column=Column(String(50), CheckConstraint("role IN ('owner', 'editor', 'viewer')"))
     )
+    created_at: datetime = Field(default_factory=get_time_stamp)
+    updated_at: datetime = Field(default_factory=get_time_stamp)
 
     # Defining the UNIQUE constraint on (team_id, user_id)
     __table_args__ = (
@@ -109,7 +122,7 @@ class Member(SQLModel, table=True):
     team: Optional["Team"] = Relationship(back_populates='members', passive_deletes=True)
     tasks: List["Task"] = Relationship(back_populates="members", link_model=TaskMemberLink,
                                        passive_deletes=True)
-    events: List["Event"] = Relationship(back_populates="members", link_model=EvenMemberLink,
+    events: List["Event"] = Relationship(back_populates="members", link_model=EventMemberLink,
                                        passive_deletes=True)
 
 
@@ -123,6 +136,7 @@ class Project(SQLModel, table=True):
         sa_column=Column(ForeignKey("teams.id", ondelete="CASCADE"))
     )
     created_at: datetime = Field(default_factory=get_time_stamp)
+    updated_at: datetime = Field(default_factory=get_time_stamp)
 
     # Relationships
     team: Optional["Team"] = Relationship(back_populates='projects', passive_deletes=True)
@@ -140,6 +154,7 @@ class Board(SQLModel, table=True):
         sa_column=Column(ForeignKey("projects.id", ondelete="CASCADE"))
     )
     created_at: datetime = Field(default_factory=get_time_stamp)
+    updated_at: datetime = Field(default_factory=get_time_stamp)
 
     # Relationships
     project: Optional["Project"] = Relationship(back_populates='boards', passive_deletes=True)
@@ -156,6 +171,7 @@ class BoardList(SQLModel, table=True):
     )
     position: int = Field(index=True, sa_column_kwargs={"nullable": False})
     created_at: datetime = Field(default_factory=get_time_stamp)
+    updated_at: datetime = Field(default_factory=get_time_stamp)
 
     # Relationships
     board: Optional["Board"] = Relationship(back_populates='lists', passive_deletes=True)
@@ -183,6 +199,7 @@ class Task(SQLModel, table=True):
 
     due_date: datetime = Field(index=True, sa_column_kwargs={"nullable": False})
     created_at: datetime = Field(default_factory=get_time_stamp)
+    updated_at: datetime = Field(default_factory=get_time_stamp)
 
     # Relationships
     list: Optional["BoardList"] = Relationship(back_populates='tasks', passive_deletes=True)
@@ -206,9 +223,115 @@ class Event(SQLModel, table=True):
         sa_column=Column(ForeignKey("users.id", ondelete="CASCADE"))
     )
     created_at: datetime = Field(default_factory=get_time_stamp)
+    updated_at: datetime = Field(default_factory=get_time_stamp)
 
     # Relationships
     project: Optional["Project"] = Relationship(back_populates="events", passive_deletes=True)
     user: Optional["User"] = Relationship(back_populates="events", passive_deletes=True)
-    members: List["Member"] = Relationship(back_populates='events', link_model=EvenMemberLink,
+    members: List["Member"] = Relationship(back_populates='events', link_model=EventMemberLink,
                                            passive_deletes=True)
+    google_events: List["GoogleSyncedEvent"] = Relationship(back_populates="event", passive_deletes=True)
+
+
+
+
+class GoogleSyncedEvent(SQLModel, table=True):
+    __tablename__ = 'google_events'
+    id: str = Field(default_factory=lambda: str(uuid4()), primary_key=True)
+    event_id: str = Field(
+        sa_column=Column(ForeignKey("events.id", ondelete="CASCADE"))
+    )
+    user_id:  str = Field(
+        sa_column=Column(ForeignKey("users.id", ondelete="CASCADE"))
+    )
+    google_event_id: str
+    google_calendar_id: Optional[str] = Field(index=True)
+    updated_at: datetime = Field(default_factory=get_time_stamp)
+
+    __table_args__ = (
+        UniqueConstraint("event_id", "user_id", name="unique_event_user"),
+        )
+
+    # Relationships
+    user: Optional["User"] = Relationship(back_populates="google_events", passive_deletes=True)
+    event: Optional["Event"] = Relationship(back_populates="google_events", passive_deletes=True)
+
+
+
+
+class Invite(SQLModel, table=True):
+    __tablename__ = 'invites'
+    id: str = Field(default_factory=lambda: str(uuid4()), primary_key=True)
+    team_id : str = Field(
+        sa_column=Column(ForeignKey("teams.id", ondelete="CASCADE"))
+    )
+    role: str = Field(
+        default="editor",
+        max_length=50,
+        sa_column=Column(String(50), CheckConstraint("role IN ('owner', 'editor', 'viewer')"))
+    )
+    email: str = Field(index=True, max_length=255, nullable=False)
+    token: str = Field(default_factory=lambda: uuid4().hex, index=True)
+    invited_by: str = Field(
+        sa_column=Column(ForeignKey("users.id", ondelete="CASCADE"))
+    )
+    created_at: datetime = Field(default_factory=get_time_stamp)
+    updated_at: datetime = Field(default_factory=get_time_stamp)
+    expires_at: datetime = Field(default_factory=lambda: get_time_stamp() + timedelta(days=7))
+    status: str = Field(
+        default="pending",
+        max_length=50,
+        sa_column=Column(String(50), CheckConstraint("type IN ('pending', 'accepted', 'declined')"))
+    )
+    registered: bool = Field(default=False)
+
+    # Defining the UNIQUE constraint on (team_id, email)
+    __table_args__ = (
+        UniqueConstraint("team_id", "email", name="unique_team_email_invite"),
+    )
+
+    def __repr__(self):
+        return f"<Invite id={self.id} email={self.email} team_id={self.team_id}>"
+
+    @property
+    def is_expired(self) -> bool:
+        return get_time_stamp() > self.expires_at.replace(tzinfo=timezone.utc)
+
+    # Relationships
+    inviter: Optional["User"] = Relationship(back_populates="invites_sent", passive_deletes=True)
+    team: Optional["Team"] = Relationship(back_populates="invites_sent_by_members", passive_deletes=True)
+
+
+
+class Notification(SQLModel, table=True):
+    __tablename__ = 'notifications'
+    id: str = Field(default_factory=lambda: str(uuid4()), primary_key=True)
+    user_id: str = Field(
+        sa_column=Column(ForeignKey("users.id", ondelete="CASCADE"))
+    )
+    sender_id: str = Field(
+        sa_column=Column(ForeignKey("users.id", ondelete="CASCADE"))
+    )
+    object_type: str = Field(
+        max_length=50,
+        sa_column=Column(String(50), CheckConstraint("type IN ('task', 'event', 'invitation')"))
+    )
+    object_id: str = Field(index=True, max_length=255, nullable=False)
+    # status: str = Field(
+    #     default="pending",
+    #     max_length=50,
+    #     sa_column=Column(String(50), CheckConstraint("type IN ('pending', 'accepted', 'declined')"))
+    # )
+    message: Optional[str] = Field(default=None)
+    is_read: bool = Field(default=False)
+
+    created_at: datetime = Field(default_factory=get_time_stamp)
+    updated_at: datetime = Field(default_factory=get_time_stamp)
+
+
+
+
+@event.listens_for(SQLModel, "before_update", propagate=True)
+def auto_update_timestamp(_, __, target):
+    if hasattr(target, "updated_at"):
+        target.updated_at = get_time_stamp()
