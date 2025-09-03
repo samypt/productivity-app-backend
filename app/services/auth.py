282598fd6a62223e8a200ae5a8791bbd6c6688fc
samypt <vitaliiptichkin@gmail.com>
2025-09-03 14:33:52 +0200

    Improve authentication services and logic

--- app/services/auth.py
+++ app/services/auth.py
@@ -1,11 +1,14 @@
-from datetime import datetime, timedelta, timezone
+from datetime import timedelta
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
 import os
 from typing import Optional, Annotated
+from dotenv import load_dotenv
+from ..utils.time import get_time_stamp
 
 
+load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
@@ -16,9 +19,15 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/login")
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
-    expire = (datetime.now(timezone.utc) +
+    expire = (get_time_stamp() +
               (expires_delta or timedelta(minutes=float(ACCESS_TOKEN_EXPIRE_MINUTES))))
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    print("ACCESS_TOKEN_EXPIRE_MINUTES", ACCESS_TOKEN_EXPIRE_MINUTES)
+    print("expire", expire)
+    print("expires_delta", expires_delta)
+    print("time now", get_time_stamp())
     to_encode.update({"exp": expire})
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
@@ -34,11 +43,15 @@ async def get_current_user(
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         user_id: str = payload.get("id")
         username: str = payload.get("sub")
+        email: str = payload.get("email")
+        role: str = payload.get("role")
         if user_id is None or username is None:
             raise credentials_exception
         return {
             'username': username,
-            'id': user_id
+            'id': user_id,
+            'email': email,
+            'role': role,
         }
     except JWTError:
         raise credentials_exception
